b9143e4b99216e09c16a96b9102b5f28f81c881e
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-10-02 18:41:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,25 +5,8 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
-
-
-
-// const getCurrentDate = () => {   // => reprensents function
-//     const date = new Date();
-//     return date.toDateString();
-
-
-// }
-
-// const greeting = React.createElement('h1', {}, 'Hello World')  //<h1>Hello World</h1>
-// const greeting =  <h1 class="text-primary" /*style= {{ marginTop: 30 + 'px'}}*/>   Hello World! Current Date: {getCurrentDate()}  </h1> ; 
-
-// ReactDOM.render(greeting, document.getElementById('root'));
-// registerServiceWorker();
-
-
 ReactDOM.render(
-    <BrowserRouter><App /></BrowserRouter>  , 
+    <BrowserRouter><App /></BrowserRouter>, 
     document.getElementById('root')
     );
 registerServiceWorker();
